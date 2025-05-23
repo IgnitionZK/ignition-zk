0ff5c59993e3d059f57bdeb8424ef93914f9dff7
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-23 14:14:09 -0700

    create frontend directory with an empty vite react.js project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>IgnitionZK</h1>
+        <p>Anthony Spedaliere | Anastasia Tsitali</p>
+        <p>2025</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
