0e85823395c0ff8d45dd9b6be44ce134de0c03d0
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-23 19:00:23 -0700

    refactored:     - added check to generateMnemonicSeed to enforce a minimum of 256 bits

--- frontend/src/scripts/generateCredentials.js
+++ frontend/src/scripts/generateCredentials.js
@@ -1,27 +1,34 @@
 // imports
 import * as bip39 from "bip39";
 import * as circomlibjs from "circomlibjs";
+import hkdf from "futoin-hkdf";
 
 /**
  * @title Generate Seed
  * @notice Generates a cryptographically secure seed value from a BIP39 mnemonic
  * @dev Uses bip39 library to generate mnemonic and seed, then reduces it to BN254's finite field
  * @param {number} bits - The number of bits of entropy for mnemonic generation (default: 128)
- * @return {bigint} The generated seed value reduced to BN254's finite field
+ * @return {{ secretSeed: bigint, mnemonic: string }} The generated seed value reduced to BN254's finite field
  * @example
  * // Generate a seed from a 12-word mnemonic (128 bits)
  * const seed = generateSeed();
  * // Generate a seed from a 24-word mnemonic (256 bits)
  * const seed = generateSeed(256);
  */
-export function generateMnemonicSeed(bits = 128) {
+export function generateMnemonicSeed(bits = 256) {
+  if (bits < 256) {
+    throw new Error(
+      "At least 256 bits of entropy are recommended for zk-identity use."
+    );
+  }
+
   // create 12-word mnemonic and seed
   const mnemonic = bip39.generateMnemonic(bits);
   const seed = bip39.mnemonicToSeedSync(mnemonic);
 
   // Securely slice and copy
-  const seedSlice = Uint8Array.prototype.slice.call(seed, 0, 32);
-  const rawBigInt = BigInt("0x" + Buffer.from(seedSlice).toString("hex"));
+  const derivedKey = hkdf(seed, 32, { hash: "SHA-256", info: "zk-identity" });
+  const rawBigInt = BigInt("0x" + Buffer.from(derivedKey).toString("hex"));
 
   // Reduce the input into the BN254's finite field
   const FIELD_PRIME =
