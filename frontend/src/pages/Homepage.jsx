0984bc725dce3b8ca91c2816a86013fb77278568
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-27 12:30:42 -0700

    -added styling to homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,24 @@
+import styled from "styled-components";
+
+const StyledHomepage = styled.div`
+  background-color: var(--color-grey-800);
+  color: var(--color-grey-100);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 2rem;
+`;
+
 function Homepage() {
   return (
-    <>
-      <div>IgnitionZK</div>
-    </>
+    <StyledHomepage>
+      <h1>IgnitionZK</h1>
+      <p>Anthony Spedaliere | Anastasia Tsitali</p>
+      <p>© 2025</p>
+    </StyledHomepage>
   );
 }
 
